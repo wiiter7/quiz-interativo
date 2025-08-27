4825880c6b75a126b6bf5de2b4664cac3b1223e2
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-08-27 07:43:21 -0300

    s

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,96 @@
-
+  const perguntas = [
+    {
+        enunciado: "Voce é gay ?",
+        alternativas: [
+            {
+               texto: "sim",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            },
+            {
+               texto: "não tem certeza",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            }
+        ]
+    },
+     {
+        enunciado: "Voce é hetero ??",
+        alternativas: [
+            {
+               texto: "não",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            },
+            {
+               texto: "Acho que não",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            }
+        ]
+    },
+     {
+        enunciado: "Voce ja deu o caneco ?",
+        alternativas: [
+            {
+               texto: "sempre",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            },
+            {
+               texto: "não lembro",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            }
+        ]
+    },
+     {
+        enunciado: "Voce é homem?",
+        alternativas: [
+            {
+               texto: "no fundo sim",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            },
+            {
+               texto: "por fora sim",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            }
+        ]
+    }, {
+        enunciado: "seu pai sabe que voce gosta de rapazes ?",
+        alternativas: [
+            {
+               texto: "sim",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            },
+            {
+               texto: "não tenho coragem de contar",
+               afirmacao: [
+                "afirmação 1",
+                "afirmação 2"
+               ],
+            }
+        ]
+    }
+  ]
