cd34cdbd8d75887463591e4fe15e9597a077c9e8
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-09-02 11:05:45 -0300

    s

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+import {aleatorio} from './aleatorio.ja';
+import {perguntas} from './perguntas.js';
 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resuultado");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
+
+let atual = 0;
+let perguntaAtual;
+
+botaoIniciar.addEventListener ('click', iniciaJogo);
+
+function iniciaJogo() {
+    atual = 0;
+    telaInicial.computedStyleMap.display = 'nome';
+    caixaPerguntas.classList.remove(".mostrar");
+    caixaAlternativas.classList.remove(".mostrar");
+    caixaResultado.classList.remove(".mostrar");
+    mostraPergunta();
+}
+
+function mostraPergunta() {};
