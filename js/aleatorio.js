37f97a8fba8225110cb7bdc011fb4f0a59420927
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-09-10 07:41:22 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,5 +1,8 @@
 const nome = ["juca", "emerson", "osvaldo", "pipokinha", "deolane", "virginiea"];
 
 export function aleatorio(lista){
-    const posicao = Math.
+    const posicao = Math.floor(Math.random()+ lista.lenght;
+    return lista[posicao];
 }
+
+expor const nome = aleatorio(nomes)
